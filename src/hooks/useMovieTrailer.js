dcd8ccf9a4b7a5ddc74ddc0102717e4ce946ef26
lovely1it <lovelykumari1it@gmail.com>
2024-09-05 09:31:39 +0530

    custom hook created!

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import { addTrailorVideo } from "../utils/moviesSlice";
 
+//to get the movie trailer a custom hook created
 const useMovieTrailer = (movieId)=>{
     const dispatch = useDispatch();
     const getMovieVideos = async () => {
@@ -11,7 +12,7 @@ const useMovieTrailer = (movieId)=>{
         API_OPTIONS
       );
       const json = await data.json();
-      const filterData = json.results.filter((video) => video.type == "Trailer");
+      const filterData = json.results.filter((video) => video.type === "Trailer");
       const trailer = filterData.length ? filterData[0] : json.results[0];
       dispatch(addTrailorVideo(trailer));
     };
